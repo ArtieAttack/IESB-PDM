c0d72ac49cfde9ab1ce4eb91f5241ea3d826c4f1
ArtieAttack <artie.attack15@gmail.com>
2025-03-19 16:20:55 -0300

    Testando Commit

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="bg-white w-full h-full flex justify-center items-center gap-8">
       <Text className="text-2xl font-bold text-yellow-600">
-        Teste Cor
+        Teste Commit
       </Text>
     <Button>
       <Text className="text-white">Clique aqui</Text>
