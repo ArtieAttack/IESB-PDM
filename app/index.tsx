f366aacb09032cef083fb770372e25670758654c
ArtieAttack <artie.attack15@gmail.com>
2025-05-09 11:20:39 -0300

    Refactor code structure for improved readability and maintainability

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
-import { ScrollView, Text, View } from "react-native";
-import Cardapio from "./Cardapio";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import Header from "./components/Header";
 
 export default function Index() {
   return (
-    <ScrollView className="w-full h-full">
-      <Header />
-      <View className="flex flex-col w-full h-full p-4 bg-[#faf3d7]">
-        <Cardapio />
-      </View>
-    </ScrollView>
+    <SafeAreaView className="py-12">
+      <ScrollView className="w-full h-full">
+        <Header />
+        <View className="flex flex-col w-full h-full p-4 bg-[#faf3d7]"></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
